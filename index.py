5fce767b01e82a31d4a7de15345c4e587333a611
Spectralia <oracle.code@pm.me>
2024-09-12 17:10:19 -0700

    generateMessages API - Claude 3.5

---
+++ index.py
@@ -0,0 +1,50 @@
+import json
+import time
+import boto3  # type: ignore
+
+
+s3Bucket = "learngpt-s3"
+
+
+def main(event, context):
+    start_time = time.time()
+    response_body = "Hello from Lambda"
+    # http_method = event.get("httpMethod", None)
+    http_method = event["httpMethod"]
+    client_body = event["body"]
+    # http_method = "POST"
+    if client_body and http_method:
+        if http_method == "POST":
+            response_body = generateMessages(client_body)
+    end_time = time.time()
+    execution_time = "Program Run Time : %.2f sec" % (end_time - start_time)
+
+    return {
+        "statusCode": 200,
+        "body": "{}\n\n{}".format(execution_time, response_body),
+        "headers": {"Content-Type": "text/plain"},
+    }
+
+
+def generateMessages(client_prompt):
+    client_bedrock = boto3.client("bedrock-runtime")
+    # client_s3 = boto3.client("s3")
+    input_prompt = """{}""".format(client_prompt)
+    response_bedrock = client_bedrock.invoke_model(
+        contentType="application/json",
+        accept="application/json",
+        # modelId="stability.stable-diffusion-xl-v1",
+        modelId="anthropic.claude-3-haiku-20240307-v1:0",
+        body=json.dumps(
+            {
+                "max_tokens": 256,
+                "messages": [{"role": "user", "content": input_prompt}],
+                "anthropic_version": "bedrock-2023-05-31",
+            }
+        ),
+    )
+    response_body = json.loads(response_bedrock["body"].read())
+    finish_reason = response_body.get("error")
+    if finish_reason is not None:
+        raise Exception(f"Text generation error. Error is {finish_reason}")
+    return response_body["content"][0]["text"]
