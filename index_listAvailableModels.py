dce32dd3aaf1e907949508e02913fe42a8548365
Spectralia <oracle.code@pm.me>
2024-09-12 17:16:47 -0700

    init

---
+++ index_listAvailableModels.py
@@ -0,0 +1,19 @@
+import json
+import boto3 # type: ignore
+
+# import time
+
+
+def main(event, context):
+    response_body = listAvailableModels(event, context)
+    return {"statusCode": 200, "body": json.dumps(response_body)}
+
+
+
+def listAvailableModels(event, context):
+    client_bedrock = boto3.client("bedrock")
+    response = client_bedrock.list_foundation_models()
+    response_body = []
+    for summary in response["modelSummaries"]:
+        response_body.append({summary["providerName"]: summary["modelId"]})
+    return response_body
